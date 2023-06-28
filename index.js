3611a7fba71a0a0b344d32549ff04b3ebd737cb2
Jascha <jascha3600@gmail.com>
2023-06-28 14:18:20 +0200

    added streaming key validation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const NodeMediaServer = require("node-media-server");
 var ffmpeg = require("fluent-ffmpeg");
-const http = require("http");
+const axios = require("axios");
+var CryptoJS = require("crypto-js");
+const crypto = require("nodejs-jsencrypt");
 // const fs = require("fs");
 // var ffmpegPath = "C:\\ffmpeg\\bin\\ffmpeg.exe";
 // if (fs.existsSync(ffmpegPath)) {
@@ -61,7 +63,7 @@ const config = {
                 hls: true,
                 hlsFlags:
                     "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
-                hlsKeep: false, // to prevent hls file delete after end the stream
+                hlsKeep: true, // to prevent hls file delete after end the stream
                 // dash: true,
                 // dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
                 // dashKeep: false, // to prevent dash file delete after end the stream
@@ -75,11 +77,65 @@ var nms = new NodeMediaServer(config);
 //     console.log("postPublish");
 // });
 
-// nms.on("prePublish", (id, StreamPath, args) => {
-//     console.log("prePublish");
-//     console.log(StreamPath);
-//     console.log(args.sign);
-//     //nms.stop();
-// });
+nms.on("prePublish", (id, StreamPath, args) => {
+    const streamKey = args.sign;
+    const signature = streamKey.split("-")[0].replaceAll(" ", "+");
+    const timeStamp = streamKey.split("-")[1];
+    const userName = StreamPath.split("/")[2];
+    console.log(signature);
+    console.log(timeStamp);
+    console.log(userName);
+    var jsencrypt = new crypto.JSEncrypt();
+    jsencrypt.setPublicKey(
+        "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCXn1KW/Kxp3+1olLQuH7p71io4C0CKF61GySLQ29kOQ6uDQGmDLYof9oquHgAQp/ZHm/jVnthdmXG8iO4i8I/lIi1aQn34I9wn6HmBMRNUai59UuQlyPZDCTWk7EFz0gPwZ46aV/uWdMwCPk2Xn+OAejcESLBP3WG9HbTHo0/lQIDAQAB"
+    );
+
+    const originalData = {
+        userName: userName,
+        timeStamp: timeStamp,
+    };
+
+    const request = {
+        originalData: originalData,
+        signature: signature,
+    };
+
+    console.log(request);
+    var isSameUser = false;
+    axios
+        .post("http://localhost:5291/api/VideoStream/ValidateStream", request)
+        .then(function (response) {
+            //console.log(response.data);
+            if (
+                jsencrypt.verify(
+                    response.data.originalData,
+                    response.data.signature,
+                    CryptoJS.SHA256
+                )
+            ) {
+                isSameUser = response.data.originalData;
+            }
+
+            if (!isSameUser) {
+                nms.stop();
+            }
+        })
+        .catch(function (error) {
+            console.log("BAD REQUEST");
+            nms.stop();
+        });
+
+    //nms.stop();
+});
 
 nms.run();
+// const henk = {
+//     henk: "henk",
+// };
+// const verified = test.verify(
+//     JSON.stringify(henk, null, 0).toLowerCase(),
+//     "lKN7STf/rifCukQWEAY0BLkWv0OTITN0OBwd85WKp9zMV2KCQ7gEZCAESPBIreXi38Vx5tYqPs19CSQ/yMyglbJ794wjtDb9MM6kBrVRGJbKSsP96KsDPRs/cFRaQqcXEX25mtRPS+bZZI7gVMJde0/RDdkL0SlHI/YmJTCUIoc=",
+//     CryptoJS.SHA256
+// );
+// console.log("EXPECTING TRUE");
+// console.log(verified);
